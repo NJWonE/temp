96ff57005aa55df4679acf4871b4a4915225bb9e
noh <nohjaewon123@daum.net>
2023-03-02 22:33:05 +0900

    side Project

---
+++ src/main/java/noh/jae/won/temp/configuration/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package noh.jae.won.temp.configuration.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/v1/members/signup", "/api/v1/members/signin").permitAll()
+                        .anyRequest().hasAnyRole("admin", "user"))
+                .cors()
+                .disable();
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://locahost:5555"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("POST", "UPDATE", "DELETE", "GET"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return urlBasedCorsConfigurationSource;
+    }
+}
---
+++ src/main/java/noh/jae/won/temp/configuration/config/TempApplication.java
@@ -0,0 +1,13 @@
+package noh.jae.won.temp.configuration.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempApplication.class, args);
+    }
+
+}
